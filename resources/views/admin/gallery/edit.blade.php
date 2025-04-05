45f4ea6fb28b50b288ff2c674eb933dc65ae5f5e
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-04-05 11:35:10 +0500

    `fix: input old value handling for gallery edit form`

--- resources/views/admin/gallery/edit.blade.php
+++ resources/views/admin/gallery/edit.blade.php
@@ -32,7 +32,7 @@
         <div class="row">
             <div class="form-group col-lg">
                 <label for="register-name[uz]" class="form-label"><b>{{__('Name')}} (uz)</b></label>
-                <input value="{{ $item->name }}" type="text" class="form-control @error('name[uz]') is-invalid @enderror" id="register-name[uz]" name="name[uz]" placeholder="name[uz]" aria-describedby="register-name[uz]" tabindex="1" autofocus value="{{ old('name[uz]') }}" />
+                <input value="{{ old('name.uz', $item->name->uz ?? '') }}" type="text" class="form-control @error('name[uz]') is-invalid @enderror" id="register-name[uz]" name="name[uz]" placeholder="name[uz]" aria-describedby="register-name[uz]" tabindex="1" />
                 @error('name[uz]')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -41,7 +41,7 @@
             </div>
             <div class="form-group col-lg">
                 <label for="register-name[ru]" class="form-label"><b>{{__('Name')}} (ru)</b></label>
-                <input value="{{ $item->name }}" type="text" class="form-control @error('name[ru]') is-invalid @enderror" id="register-name[ru]" name="name[ru]" placeholder="name[ru]" aria-describedby="register-name[ru]" tabindex="1" autofocus value="{{ old('name[ru]') }}" />
+                <input value="{{ old('name.ru', $item->name->ru ?? '') }}" type="text" class="form-control @error('name[ru]') is-invalid @enderror" id="register-name[ru]" name="name[ru]" placeholder="name[ru]" aria-describedby="register-name[ru]" tabindex="1" />
                 @error('name[ru]')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -50,7 +50,7 @@
             </div>
             <div class="form-group col-lg">
                 <label for="register-name[en]" class="form-label"><b>{{__('Name')}} (en)</b></label>
-                <input value="{{ $item->name }}" type="text" class="form-control @error('name[en]') is-invalid @enderror" id="register-name[en]" name="name[en]" placeholder="name[en]" aria-describedby="register-name[en]" tabindex="1" autofocus value="{{ old('name[en]') }}" />
+                <input value="{{ old('name.en', $item->name->en ?? '') }}" type="text" class="form-control @error('name[en]') is-invalid @enderror" id="register-name[en]" name="name[en]" placeholder="name[en]" aria-describedby="register-name[en]" tabindex="1" />
                 @error('name[en]')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -61,7 +61,7 @@
         <div class="row">
             <div class="form-group col-lg">
                 <label for="register-description[uz]" class="form-label"><b>{{__('Description')}} (uz)</b></label>
-                <input value="{{ $item->description }}" type="text" class="form-control @error('description[uz]') is-invalid @enderror" id="register-description[uz]" name="description[uz]" placeholder="description[uz]" aria-describedby="register-description[uz]" tabindex="1" autofocus value="{{ old('description[uz]') }}" />
+                <input value="{{ old('description.uz', $item->description->uz ?? '') }}" type="text" class="form-control @error('description[uz]') is-invalid @enderror" id="register-description[uz]" name="description[uz]" placeholder="description[uz]" aria-describedby="register-description[uz]" tabindex="1" />
                 @error('description[uz]')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -70,7 +70,7 @@
             </div>
             <div class="form-group col-lg">
                 <label for="register-description[ru]" class="form-label"><b>{{__('Description')}} (ru)</b></label>
-                <input value="{{ $item->description }}" type="text" class="form-control @error('description[ru]') is-invalid @enderror" id="register-description[ru]" name="description[ru]" placeholder="description[ru]" aria-describedby="register-description[ru]" tabindex="1" autofocus value="{{ old('description[ru]') }}" />
+                <input value="{{ old('description.ru', $item->description->ru ?? '') }}" type="text" class="form-control @error('description[ru]') is-invalid @enderror" id="register-description[ru]" name="description[ru]" placeholder="description[ru]" aria-describedby="register-description[ru]" tabindex="1" />
                 @error('description[ru]')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -79,7 +79,7 @@
             </div>
             <div class="form-group col-lg">
                 <label for="register-description[en]" class="form-label"><b>{{__('Description')}} (en)</b></label>
-                <input value="{{ $item->description }}" type="text" class="form-control @error('description[en]') is-invalid @enderror" id="register-description[en]" name="description[en]" placeholder="description[en]" aria-describedby="register-description[en]" tabindex="1" autofocus value="{{ old('description[en]') }}" />
+                <input value="{{ old('description.en', $item->description->en ?? '') }}" type="text" class="form-control @error('description[en]') is-invalid @enderror" id="register-description[en]" name="description[en]" placeholder="description[en]" aria-describedby="register-description[en]" tabindex="1" />
                 @error('description[en]')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
@@ -88,7 +88,7 @@
             </div>
         </div>
         <div class="row justify-content-center" style="margin-left: 0px; margin-right: 0px">
-          <button type="submit" class="btn btn-primary btn-block col-12" tabindex="5"><b>{{__('Add')}}</b></button>
+          <button type="submit" class="btn btn-primary btn-block col-12" tabindex="5"><b>{{__('Update')}}</b></button>
         </div>
         </form>
       </div>
